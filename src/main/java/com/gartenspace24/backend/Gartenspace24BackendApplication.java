507a78ba271d6acdd6c4c39a994e22b2acb6ade6
Tom Bischopink <s87622@informatik.htw-dresden.de>
2025-07-27 18:14:56 +0200

    Initial Commit2

---
+++ src/main/java/com/gartenspace24/backend/Gartenspace24BackendApplication.java
@@ -0,0 +1,13 @@
+package com.gartenspace24.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gartenspace24BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gartenspace24BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gartenspace24/backend/Gartenspace24BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gartenspace24.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gartenspace24BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
